d4743a2bc06a622778380403c1613ad4c80ac603
Alex Ewerlöf <hanifbbz@gmail.com>
2020-09-05 23:02:25 +0200

    read and write files in parallel

--- index.js
+++ index.js
@@ -24,37 +24,35 @@ function getToken(source) {
     }
 }
 
-function fetchFiles(config) {
-    return Promise.all(config.map(async ({ source, localFilePath }) => {
-        const translatedSource = whereFrom(source)
-        const token = getToken(source)
-        const headers = {}
-        if (token) {
-            headers['Authorization'] = `token ${token}`
-        }
-        const response = await fetch(translatedSource, { headers })
-        if (!response.ok) {
-            throw new Error(`Failed to fetch ${source} from ${translatedSource}. Error: ${response.status} ${response.statusText}`)
-        }
-        console.log(`Fetched ${source}`)
-        return {
-            source,
-            localFilePath,
-            contents: await response.text()
-        }
-    }))
+async function readSource(source) {
+    const translatedSource = whereFrom(source)
+    const token = getToken(source)
+    const headers = {}
+    if (token) {
+        headers['Authorization'] = `token ${token}`
+    }
+    const response = await fetch(translatedSource, { headers })
+    if (!response.ok) {
+        throw new Error(`Failed to fetch ${source} from ${translatedSource}. Error: ${response.status} ${response.statusText}`)
+    }
+    console.log(`Fetched ${source}`)
+    return await response.text()
 }
 
-function writeFiles(config) {
-    return Promise.all(config.map(async ({ localFilePath, contents }) => {
-        const dir = path.dirname(localFilePath)
-        if (dir !== '.') {
-            console.log(`Creating dir ${dir}...`)
-            await mkdir(dir, { recursive: true })
-        }
-        console.log(`Writing file ${localFilePath}...`)
-        return writeFile(localFilePath, contents)
-    }))
+async function writeLocalFile(localFilePath, contents) {
+    const dir = path.dirname(localFilePath)
+    if (dir !== '.') {
+        console.log(`Creating dir ${dir}...`)
+        await mkdir(dir, { recursive: true })
+    }
+    console.log(`Writing file ${localFilePath}...`)
+    return writeFile(localFilePath, contents)
+}
+
+async function readSourceWriteFile({ source, localFilePath }) {
+    const contents = await readSource(source)
+    console.log('ho')
+    return await writeLocalFile(localFilePath, contents)
 }
 
 async function main() {
@@ -64,7 +62,7 @@ async function main() {
         return
     }
     console.table(config)
-    await writeFiles(await fetchFiles(config))
+    await Promise.all(config.map(readSourceWriteFile))
 }
 
 module.exports = { main }
